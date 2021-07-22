aaf6d9d156ebf075c71c29bc0e94b21480a4b7a6
Vlad <ilin96inbox@gmail.com>
2021-07-22 13:05:33 +0900

    add email validation

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create and save a new user"""
+        if not email:
+          raise ValueError('User must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
