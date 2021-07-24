244869e26c6e131d7c254819995fb5a08bb59a71
Vlad <ilin96inbox@gmail.com>
2021-07-24 10:53:11 +0900

    add super user

--- app/core/models.py
+++ app/core/models.py
@@ -6,9 +6,18 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create and save a new user"""
         if not email:
-          raise ValueError('User must have an email address')
+            raise ValueError('User must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self.db)
 
         return user
